8585cb0c5ee75045f09a723284680e272cd26fbc
Qiuyang Nie <qyang.nie@gmail.com>
2020-07-11 17:52:10 +0100

    Construct Payload

--- auth-impl/src/main/scala/com/prototype/auth/impl/AuthServiceImpl.scala
+++ auth-impl/src/main/scala/com/prototype/auth/impl/AuthServiceImpl.scala
@@ -4,11 +4,28 @@ import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.prototype.auth.api.AuthService
 
-import scala.concurrent.{ExecutionContext, Future}
+import play.api.libs.json.{JsValue, Json}
 
-import com.prototype.auth.api.model.TokenRequest
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+
+import java.time.Instant
+
+import com.prototype.auth.api.model.{TokenRequest, User, Payload}
 
 class AuthServiceImpl(implicit ec: ExecutionContext) extends AuthService {
+
+  private def payload(user: User): String = {
+    val jwtPayload = Payload(
+      issuer     = "qiuyang",
+      subject    = user.principal,
+      audience   = "WebApplication",
+      expiration = Instant.now.getEpochSecond + Duration("30 minutes").toSeconds
+    )
+
+    val payloadJson: JsValue = Json.toJson(jwtPayload)
+    payloadJson.toString()
+  }
 
   override def token(): ServiceCall[TokenRequest, NotUsed] = ServiceCall(_ => Future(NotUsed))
 
