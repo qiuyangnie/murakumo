5b791ffe88b1c66827a4fba8332a4f68c342e374
Qiuyang Nie <qyang.nie@gmail.com>
2020-07-13 17:42:57 +0100

    Declare a file topic

--- discovery-api/src/main/scala/com/prototype/discovery/api/DiscoveryService.scala
+++ discovery-api/src/main/scala/com/prototype/discovery/api/DiscoveryService.scala
@@ -3,6 +3,13 @@ package com.prototype.discovery.api
 import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.transport.Method
 import com.lightbend.lagom.scaladsl.api.{Service, Descriptor, ServiceAcl, ServiceCall}
+import com.lightbend.lagom.scaladsl.api.broker.Topic
+
+import com.prototype.discovery.api.model.File
+
+object DiscoveryService {
+  val file_TOPIC_NAME = "file"
+}
 
 trait DiscoveryService extends Service {
 
@@ -12,12 +19,19 @@ trait DiscoveryService extends Service {
       .withCalls(
         pathCall("/discovery/produce", () => produceMessage())
       )
+      .withTopics(
+        topic(DiscoveryService.file_TOPIC_NAME, fileTopic)
+      )
       .withAutoAcl(true)
       .withAcls(
         ServiceAcl.forMethodAndPathRegex(Method.OPTIONS, "/.*")
       )
   }
 
+  // The topic handle:
+  def fileTopic(): Topic[File]
+
+  // The call handle:
   def produceMessage(): ServiceCall[NotUsed, NotUsed]
 
 }
