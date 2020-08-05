83a30be1af9e9cc84382fc6107f5ed93a26d1fa3
Qiuyang Nie <qyang.nie@gmail.com>
2020-08-05 21:32:38 +0100

    Scaffold first test

--- auth-impl/src/test/scala/com/prototype/auth/impl/StringHashSpec.scala
+++ auth-impl/src/test/scala/com/prototype/auth/impl/StringHashSpec.scala
@@ -1,7 +1,30 @@
 package com.prototype.auth.impl
 
-import org.scalatest.AnyFlatSpec
+import com.github.t3hnar.bcrypt._
 
-class StringHashSpec extends AnyFlatSpec{
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.util.Try
+
+class StringHashSpec extends AnyFlatSpec {
+
+  "Passwords" should "never be stored in database" in {
+
+    val username: String = "admin"
+    val password: String = "farm1990M0O"
+
+    val passwordHash: Try[String] = StringHash.hashing(password)
+    println(passwordHash.get)
+
+
+    val passwordAgainst: String = "farm1990M0O"
+    val result: Try[Boolean] = passwordAgainst.isBcryptedSafeBounded(passwordHash.get)
+    println(result.get)
+
+    val passwordAgainst1: String = "farm1990M0"
+    val result1: Try[Boolean] = passwordAgainst1.isBcryptedSafeBounded(passwordHash.get)
+    println(result1.get)
+    
+  }
 
 }
