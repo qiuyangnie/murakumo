c62248f72df2f792559d771b74c1994df294c378
Qiuyang Nie <qyang.nie@gmail.com>
2020-08-05 21:04:53 +0100

    Scaffold implementation of method hashing

--- auth-impl/src/main/scala/com/prototype/auth/impl/StringHash.scala
+++ auth-impl/src/main/scala/com/prototype/auth/impl/StringHash.scala
@@ -1,7 +1,11 @@
 package com.prototype.auth.impl
 
+import com.github.t3hnar.bcrypt._
+
+import scala.util.Try
+
 object StringHash {
   
-  def hashing(string: String) = ???
-  
+  def hashing(string: String): Try[String] = string.bcryptSafeBounded
+
 }
