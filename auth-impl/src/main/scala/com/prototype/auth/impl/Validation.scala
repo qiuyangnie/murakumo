cfe9efc29d67e387b2c6c2ae428f71cbfce1ea27
Qiuyang Nie <qyang.nie@gmail.com>
2020-08-09 12:26:15 +0100

    Scaffold impl of Validation and ValidationSpec

--- auth-impl/src/main/scala/com/prototype/auth/impl/Validation.scala
+++ auth-impl/src/main/scala/com/prototype/auth/impl/Validation.scala
@@ -2,8 +2,14 @@ package com.prototype.auth.impl
 
 object Validation {
 
-  def validateUsername(username: String): Boolean = ???
+  def validateUsername(username: String): Boolean = {
+    val pattern: String = raw"\w{1,36}"
+    username.matches( pattern )
+  }
 
-  def validatePassword(password: String): Boolean = ???
+  def validatePassword(password: String): Boolean = {
+    val pattern: String = raw".{8,}"
+    password.matches( pattern )
+  }
 
 } 
--- auth-impl/src/test/scala/com/prototype/auth/impl/ValidationSpec.scala
+++ auth-impl/src/test/scala/com/prototype/auth/impl/ValidationSpec.scala
@@ -4,4 +4,31 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class ValidationSpec extends AnyFlatSpec {
 
+  "username" should "alpha-numeric only, maximum 36 characters, and not be empty" in {
+    assert( Validation.validateUsername( "hello" )   == true )
+    assert( Validation.validateUsername( "admin" )   == true )
+    assert( Validation.validateUsername( "gayatri" ) == true )
+    assert( Validation.validateUsername( "qiuyang" ) == true )
+    assert( Validation.validateUsername( "Qiuyang" ) == true )
+    assert( Validation.validateUsername( "123" )     == true )
+    assert( Validation.validateUsername( "" )        == false )
+    assert( Validation.validateUsername( "*&^%^$&" ) == false )
+    assert( Validation.validateUsername( "adminadminadminadminadminadminadminaaadsfsdg" ) == false )
+  }
+
+  "Password" should "minimum 8 characters" in {
+    assert( Validation.validatePassword( "hello" )   == false )
+    assert( Validation.validatePassword( "admin" )   == false )
+    assert( Validation.validatePassword( "gayatri" ) == false )
+    assert( Validation.validatePassword( "qiuyang" ) == false )
+    assert( Validation.validatePassword( "Qiuyang" ) == false )
+    assert( Validation.validatePassword( "123" )     == false )
+    assert( Validation.validatePassword( "" )        == false )
+    assert( Validation.validatePassword( "*&^%^$&" ) == false )
+    assert( Validation.validatePassword( "1234567" ) == false )
+    assert( Validation.validatePassword( "adminadminadminadminadminadminadminaaadsfsdg" ) == true )
+    assert( Validation.validatePassword( "12345678" ) == true )
+    assert( Validation.validatePassword( "        " ) == true )
+  }
+
 } 
