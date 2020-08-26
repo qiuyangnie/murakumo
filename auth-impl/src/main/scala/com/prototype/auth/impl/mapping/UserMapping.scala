db34cae25f91f851c2061980e9e2a6fcd4c4d1db
Qiuyang Nie <qyang.nie@gmail.com>
2020-08-26 19:29:27 +0100

    Add Users model table

--- auth-impl/src/main/scala/com/prototype/auth/impl/mapping/UserMapping.scala
+++ auth-impl/src/main/scala/com/prototype/auth/impl/mapping/UserMapping.scala
@@ -3,9 +3,22 @@ package com.getvisibility.auth.impl.mapping
 import play.api.Configuration
 
 import slick.jdbc.JdbcBackend.Database
+import slick.lifted.Tag
+import slick.lifted.ProvenShape
+import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.ExecutionContext
 
+// Functional Relational Mapping
 class UserMapping(db: Database, config: Configuration)(implicit ex: ExecutionContext) {
+
+  private final class Users(tag: Tag) extends Table[(Int, String, String)](tag, "USERS") {
+    def id: Rep[Int]          = column[Int]("ID", O.PrimaryKey)
+    def username: Rep[String] = column[String]("USERNAME")
+    def password: Rep[String] = column[String]("PASSWORD")
+    override def * = (id, username, password)
+  }
+
+  private val users: TableQuery[UserMapping.this.Users] = TableQuery[Users]
 
 }
