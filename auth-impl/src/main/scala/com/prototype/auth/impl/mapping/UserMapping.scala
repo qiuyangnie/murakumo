228275ac41b7e01e1d080f47cd74b7cff1facdbe
Qiuyang Nie <qyang.nie@gmail.com>
2020-08-26 19:14:24 +0100

    Scaffold UserMapping

---
+++ auth-impl/src/main/scala/com/prototype/auth/impl/mapping/UserMapping.scala
@@ -0,0 +1,11 @@
+package com.getvisibility.auth.impl.mapping
+
+import play.api.Configuration
+
+import slick.jdbc.JdbcBackend.Database
+
+import scala.concurrent.ExecutionContext
+
+class UserMapping(db: Database, config: Configuration)(implicit ex: ExecutionContext) {
+
+}
